0a482069a6b569b187ef8c459678b66f3aaf4f37
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-03-22 18:39:08 +0200

    fix the first commit issue

--- utility/get_github_secrets.py
+++ utility/get_github_secrets.py
@@ -11,7 +11,7 @@ def create_tokens_file(dictionary, filename='tokens.json'):
 
 def main():
     env_cookies = json.loads(os.environ['SECRET'])
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    cur_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     config = os.path.join(cur_dir, "config/secrets.json")
     create_tokens_file(env_cookies, config)
 
