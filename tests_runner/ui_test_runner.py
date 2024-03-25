c5c2b09de7abb1a2d09c0eb072f026f056d9667f
AhmdBdran1 <ahmdbdran@h-MacBook-Air-sl-user.local>
2024-03-25 23:41:52 +0200

    test runner fixed!

--- tests_runner/ui_test_runner.py
+++ tests_runner/ui_test_runner.py
@@ -3,8 +3,7 @@ import HtmlTestRunner
 
 from tests.ui_test.board_page_test import BoardPageTests
 from tests.ui_test.card_page_test import CardPageTests
-from tests.ui_test.home_page_test import HomePageTests
-from tests.ui_test.login_page_test import LoginPageTests
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
