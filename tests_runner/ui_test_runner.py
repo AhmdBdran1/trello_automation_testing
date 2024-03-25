ab8f33bd4baa4711dc695bfe2e78bf805c905776
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-03-25 09:48:19 +0200

    fix test runner

--- tests_runner/ui_test_runner.py
+++ tests_runner/ui_test_runner.py
@@ -7,7 +7,7 @@ from tests.ui_test.card_page_test import CardPageTests
 if __name__ == "__main__":
     suite = unittest.TestSuite()
     suite.addTest(BoardPageTests('test_all_tests'))
-    suite.addTest(CardPageTests('terst_all_tests'))
+    suite.addTest(CardPageTests('test_all_tests'))
 
     runner = HtmlTestRunner.HTMLTestRunner(output='test-reports')
     runner.run(suite)
