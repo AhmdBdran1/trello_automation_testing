cc477a4ed7a57ec3f59d2a0fc91a29cb9e8b8c50
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-03-27 20:14:44 +0200

    last simulation before my presentation

--- tests_runner/api_test_runner.py
+++ tests_runner/api_test_runner.py
@@ -1,9 +1,9 @@
-
 from tests.api_test.board_test import BoardTest
 from tests.api_test.card_test import CardTest
 from infra.api_wrapper import APIWrapper
 
 
+# run last simulation
 def main():
     test_cases = [BoardTest, CardTest]
     my_api = APIWrapper()
